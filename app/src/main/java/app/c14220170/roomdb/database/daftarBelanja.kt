bf32658c3d05b9a3566a775bf5f15c142b5ee169
C14220170 <C14220170@john.petra.ac.id>
2024-12-03 18:57:27 +0700

    database room done

---
+++ app/src/main/java/app/c14220170/roomdb/database/daftarBelanja.kt
@@ -0,0 +1,24 @@
+package app.c14220170.roomdb.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class daftarBelanja(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "Id")
+    val id: Int = 0,
+
+    @ColumnInfo(name = "tanggal")
+    val tanggal: String? = null,
+
+    @ColumnInfo(name = "item")
+    val item: String? = null,
+
+    @ColumnInfo(name = "jumlah")
+    val jumlah: String? = null,
+
+    @ColumnInfo(name = "status")
+    val status: Int = 0,
+)
